a926249c489a1da744b72f2bf29dacacb74bd834
mmkaram <mahdykaram@outlook.com>
2024-03-17 21:15:29 -0700

    fixed python linting mainly

--- markdown/md_2_html.py
+++ markdown/md_2_html.py
@@ -7,6 +7,7 @@ import os
 import re
 
 def convert():
+    '''goes through the markdown file and converts it to HTML based on my setup'''
     if len(sys.argv) != 3:
         print("Usage: python3 MDToHTML.py [input file] [output file]")
         sys.exit(1)
@@ -31,7 +32,7 @@ def convert():
         f.write("<link href='https://fonts.googleapis.com/css?family=League Spartan' rel='stylesheet'>")
         f.write("<link rel=\"stylesheet\" href=\"all.css\">\n")
         f.write("<link rel=\"icon\" href=\"icon.ico\">")
-        
+
         # script tag for copyright footer
         f.write("<script> document.addEventListener('DOMContentLoaded', function() {var currentYear = new Date().getFullYear(); var footerYear = document.getElementById('footer-year');footerYear.textContent = currentYear;});</script>")
 
@@ -71,7 +72,7 @@ def convert():
 
             if line.startswith('> '):
                 line = '<blockquote>' + line[2:] + '</blockquote>'
-            
+
             list_pattern = r'^[\t ]*- (.*)$'
             line = re.sub(list_pattern, r'<li>\1</li>', line)
 
@@ -79,7 +80,7 @@ def convert():
             line = re.sub(inline_code_pattern, r'<code>\1</code>', line)
 
 
-            
+
             if line.startswith('# '):
                 line = '<h1>' + line[2:] + '</h1>'
             elif line.startswith('## '):
@@ -92,7 +93,7 @@ def convert():
                 line = '<h5>' + line[6:] + '</h5>'
             elif line.startswith('###### '):
                 line = '<h6>' + line[7:] + '</h6>'
-            
+
             f.write(line + "\n")
         f.write("<footer>&copy; <span id=\"footer-year\"></span> Mahdy M. Karam</footer>")
         f.write("</p></body>\n</html>")
